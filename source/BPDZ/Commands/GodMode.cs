0756319c1be176d6344bff14898c3384a53c4dd8
Unlucky <47713540+Unluckyx@users.noreply.github.com>
2019-03-05 21:47:37 +1100

    Updated file system

---
+++ source/BPDZ/Commands/GodMode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+using BP_API;
+
+namespace BPDZ
+{
+    class GodMode
+    {
+        public static string GodListFile = @"BPDayZ/Godlist.txt";
+        public static int PermissionLevel = 1;
+
+        public static void Run(string username)
+        {
+            if (File.ReadAllText(GodListFile).Contains(username))
+            {
+                List<string> allusers = File.ReadAllText(GodListFile).Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+                allusers.Add(username);
+
+                File.WriteAllText(GodListFile, allusers.ToString());
+            }
+
+            else
+            {
+                List<string> allusers = File.ReadAllText(GodListFile).Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+
+                allusers.Remove(username);
+                foreach (var user in allusers)
+                {
+                    if (!user.Contains(" "))
+                    {
+                        File.WriteAllText(GodListFile, user);
+                    }
+                }
+            }
+        }
+
+        public static bool HasGodmode(Player instance)
+        {
+            string playerlist = File.ReadAllText(GodListFile);
+            if (playerlist.Contains(instance.Username))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- source/BPDZ/DayZManager.cs
+++ source/BPDZ/DayZManager.cs
@@ -9,32 +9,17 @@ using System.IO;
 using BP_API;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using BPDZ.Commands;
 
 namespace BPDZ
 {
     public class DayZManager
-    {
-
-
-
-        public static List<string> ListOfCommands = new List<string>
+    {        
+        public static void SvGlobalChatMessage(Player player, string message)
         {
-            @"/god [username] - toggles godmode for the player",
-            @"/help - displays the list of commands",
-            @"/home - teleports you to your house",
-            @"/safezone - teleports you to the nearest safezone",
-            @"/discord - shows the discord link for PPServers",
-            @"",
-            @"<color=yellow>More to come</color>"
-        };
-
-        
-        public static void SvGlobalChatMessage(Player instance, string message)
-        {
-
             string groupscolor = "";
             string groupsdisplayname = "";
-            List<DayZGroups> groups = GroupGrabber(instance.svPlayer, false).OrderByDescending(o => o.PermissionLevel).ToList();
+            List<DayZGroups> groups = GroupGrabber(player, false).OrderByDescending(o => o.PermissionLevel).ToList();
             foreach (var c in groups)
             {
                 groupsdisplayname = $"{groupsdisplayname} {c.DisplayName}";
@@ -43,35 +28,35 @@ namespace BPDZ
             
             if (message.StartsWith("/"))
             {
-                Debug.Log($"[BPDayZLogger] Command sent: [{instance.svPlayer.player.ID}]  {instance.svPlayer.player.username}: {message}");
-                SearchCommand(instance.svPlayer, message);
+                Debug.Log($"[BPDayZLogger] Command sent: [{player.svPlayer.player.ID}]  {player.svPlayer.player.username}: {message}");
+                SearchCommand(player, message);
                 return;
             }
 
-            else if (!instance.svPlayer.svManager.chatted.OverLimit(instance.svPlayer.player))
+            else if (!player.svPlayer.svManager.chatted.OverLimit(player.svPlayer.player))
             {                
-                Debug.Log($"[BPDayZLogger] Message sent: [{instance.svPlayer.player.ID}]  {instance.svPlayer.player.username}: {message}");
-                instance.svPlayer.svManager.chatted.Add(instance.svPlayer.player);
-                instance.svPlayer.Send(SvSendType.All, Channel.Unsequenced, ClPacket.GameMessage, $" {instance.svPlayer.player.username}: {message}");
+                Debug.Log($"[BPDayZLogger] Message sent: [{player.svPlayer.player.ID}]  {player.svPlayer.player.username}: {message}");
+                player.svPlayer.svManager.chatted.Add(player.svPlayer.player);
+                player.svPlayer.Send(SvSendType.All, Channel.Unsequenced, ClPacket.GameMessage, $" {player.svPlayer.player.username}: {message}");
                 return;
             }
         }
 
-        public static void SearchCommand(SvPlayer instance, string message)
+        public static void SearchCommand(Player player, string message)
         {
             List<string> newmessage = message.Split(' ').ToList();
-            RunCommand(instance, newmessage);
+            RunCommand(player, newmessage);
         }
 
-        public static int GetPermissionLevel(SvPlayer instance)
+        public static int GetPermissionLevel(Player player)
         {
             int result = 0;
-            if(instance.player.admin)
+            if(player.shPlayer.admin)
             {
                 result = 5;
             }
             DayZGroups previous = new DayZGroups { Name = "null", DisplayName = "null", PermissionLevel = "0", Color = "null"};
-            foreach (var re in GroupGrabber(instance, false))
+            foreach (var re in GroupGrabber(player, false))
             {
                 if(Convert.ToInt32(re.PermissionLevel) < Convert.ToInt32(previous.PermissionLevel))
                 {
@@ -82,53 +67,40 @@ namespace BPDZ
             return result;
         }
 
-        public static void RunCommand(SvPlayer instance, List<string> args)
+        public static void RunCommand(Player player, List<string> args)
         {
             if (args[0] == @"/god")
             {
                 
-                if(GetPermissionLevel(instance) <= GodMode.PermissionLevel)
+                if(GetPermissionLevel(player) <= GodMode.PermissionLevel)
                 GodMode.Run(args[1]);
-                SendMessage(instance, $"<color=green>[BPDayZ]: </color><color=blue>Succesfully gave Godmode to {args[1]}</color>");
+                SendMessage(player.svPlayer, $"<color=green>[BPDayZ]: </color><color=blue>Succesfully gave Godmode to {args[1]}</color>");
                 Debug.Log($"[BPDayZ]: Successfully gave Godmode to player {args[1]}");
             }
 
             else if (args[0] == @"/help")
             {
-                string text = "";
-                foreach (var item in ListOfCommands)
-                {
-                    text = text + "\n" + item;
-                }
-                instance.Send(SvSendType.Self, Channel.Fragmented, ClPacket.ServerInfo, $"<color=green>[BPDayZ] List of commands </color> " + text);
-                return;
+                Help.SendHelpMenu(player);
             }
 
             else if (args[0] == @"/home")
             {
-                instance.Send(SvSendType.Self, Channel.Unsequenced, ClPacket.GameMessage, $"<color=green>[BPDayZ]: </color><color=red>You do not have permission to use this command</color>");
-                return;
+                player.svPlayer.Send(SvSendType.Self, Channel.Unsequenced, ClPacket.GameMessage, $"<color=green>[BPDayZ]: </color><color=red>Work in progress</color>");
             }
 
             else if (args[0] == @"/safezone")
             {
-                instance.Send(SvSendType.Self, Channel.Unsequenced, ClPacket.GameMessage, $"<color=green>[BPDayZ]: </color><color=red>You do not have permission to use this command</color>");
-                return;
+                player.svPlayer.Send(SvSendType.Self, Channel.Unsequenced, ClPacket.GameMessage, $"<color=green>[BPDayZ]: </color><color=red>Work in progress</color>");
             }
 
             else if (args[0] == @"/discord")
             {
-                StreamReader sr = new StreamReader(@"BPDayZ/DiscordLink.txt");
-                string discordlink = sr.ReadToEnd();
-                sr.Close();
-                instance.Send(SvSendType.Self, Channel.Unsequenced, ClPacket.GameMessage, $"<color=green>[BPDayZ]:</color><color=yellow> {discordlink} </color>");
-                return;
+                Discord.SendDiscordLink(player);
             }
 
             else
             {
-                SendMessage(instance, $"<color=green>[BPDayZ]: </color><color=red>Unknown Command</color> <color=yellow>\"{args[0]}\"</color><color=red>. Do /help for the list of commands</color>");
-                return;
+                SendMessage(player.svPlayer, $"<color=green>[BPDayZ]: </color><color=red>Unknown Command</color> <color=yellow>\"{args[0]}\"</color><color=red>. Do /help for the list of commands</color>");
             }
         }
 
@@ -137,49 +109,25 @@ namespace BPDZ
             instance.Send(SvSendType.Self, Channel.Unsequenced, ClPacket.GameMessage, $"{message}");
         }
 
-        public static string ReadData(string location)
-        {
-            StreamReader sr = new StreamReader(location);
-            string data = sr.ReadToEnd();
-            sr.Close();
-            return data;
-        }
-
-        public static void WriteData(string location, string text)
-        {
-            StreamReader sr = new StreamReader(location);
-            string alldata = sr.ReadToEnd();
-            sr.Close();
-
-            StreamWriter sw = new StreamWriter(location);
-            
-            foreach(var c in alldata)
-            {
-                sw.WriteLine(c);
-            }
-            sw.WriteLine(text);
-            sw.Close();
-        }
-
-        public static List<DayZGroups> GroupGrabber(SvPlayer instance, bool withColor)
+        public static List<DayZGroups> GroupGrabber(Player player, bool withColor)
         {
             List<DayZGroups> ListOfGroups = new List<DayZGroups>();
             List<DayZGroups> playersGroups = new List<DayZGroups>();
             string filepath = @"BPDayZ/Groups/";
-            DirectoryInfo d = new DirectoryInfo(filepath);
+            DirectoryInfo directory = new DirectoryInfo(filepath);
 
-            foreach (var file in d.GetFiles("*.txt"))
+            foreach (var files in directory.GetFiles("*.txt"))
             {
-                List<string> Contents = ReadData($"BPDayZ/Groups/{file.Name}").Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
-                ListOfGroups.Add(new DayZGroups { Name = file.Name, DisplayName = $"[{file.Name}]", Color = $"{Contents[0]}", PermissionLevel = Contents[1] });
+                string [] Contents = File.ReadAllLines($"BPDayZ/Groups/{files.Name}");
+                ListOfGroups.Add(new DayZGroups { Name = files.Name, DisplayName = $"[{files.Name}]", Color = $"{Contents[0]}", PermissionLevel = Contents[1] });
             }
 
-            foreach (var re in ListOfGroups)
+            foreach (var groups in ListOfGroups)
             {
-                List<string> Contents = ReadData($"BPDayZ/Groups/{re.Name}").Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
-                if (Contents.Contains(instance.player.username))
+                string [] Contents = File.ReadAllLines($"BPDayZ/Groups/{groups.Name}");
+                if (Contents.Contains(player.Username))
                 {
-                    playersGroups.Add(re);
+                    playersGroups.Add(groups);
                 }
             }
             return playersGroups;
